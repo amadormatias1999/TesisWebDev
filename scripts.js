38a9fe3c0f3f43e2d3b5c19397702358cee0a7aa
matiferamador <amadormatias1999@gmail.com>
2023-10-26 19:57:41 -0300

    Se logro crear el swiper

--- scripts.js
+++ scripts.js
@@ -51,7 +51,14 @@ fetch("../data.json")
 
 // swiper
 var swiper = new Swiper(".mySwiper", {
+  speed: 600,
+  parallax: true,
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });

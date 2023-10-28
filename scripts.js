53eab48bde26349c60ab176410490d624277a1a9
matiferamador <amadormatias1999@gmail.com>
2023-10-28 10:02:39 -0300

    Correcion de errores, eliminacion pagina Preguntas,

--- scripts.js
+++ scripts.js
@@ -65,12 +65,8 @@ fetch("../data.json")
 var swiper = new Swiper(".mySwiper", {
   speed: 600,
   parallax: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+  loop: true,
+  autoplay: {
+    delay: 3000,
   },
 });
